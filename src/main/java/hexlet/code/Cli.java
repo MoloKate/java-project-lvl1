012a0e0646a69126f1951db88ced8fd5dc70f7da
MoloKate <screpka.spb@mail.ru>
2021-09-29 23:56:59 +0300

    step 3

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package cli;
+import java.util.Scanner;
+
+public class Cli {
+    public static void welcomeName () {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Welcome to the Brain Games!");
+        System.out.println("May I have your name?" + " ")
+        String name = in.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
