570219294d891ef7b5cfd8d7bc4adff161907d9e
MoloKate <screpka.spb@mail.ru>
2021-10-01 17:15:33 +0300

    step 3 complete

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,10 +1,11 @@
 package hexlet.code;
-import java.util.Scanner;
+
+
 import cli.Cli;
 
 public class App {
     public static void main(String[] args) {
 
-        Cli.welcomeName   ();
+        Cli.welcomeName();
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package cli;
 import java.util.Scanner;
 
 public class Cli {
-    public static void welcomeName () {
+    public static void welcomeName() {
         Scanner in = new Scanner(System.in);
         System.out.print("Welcome to the Brain Games!");
         System.out.println("May I have your name?" + " ");
